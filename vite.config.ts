e7d5e7d9a9efdb11d03c773f0af80bf0a738414b
aditya <aditya28jha@appinventiv.com>
2025-06-17 20:56:03 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/video_call_poc",
+  base: "/video_call_poc/",
 });
