c5e778122d3c0cf346a5a2144f55ac21c5ac649b
aditya <aditya28jha@appinventiv.com>
2025-06-17 19:38:03 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/zozo-cloud-beta",
+  base: "/video_call_poc",
 });
